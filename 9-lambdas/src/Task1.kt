ecdb038200b01dc7c315357b0e8c7cfb7227d0ec
KMV <9117479@gmail.com>
2022-03-21 12:54:05 +0300

    9 Ts1 + common file

--- 9-lambdas/src/Task1.kt
+++ 9-lambdas/src/Task1.kt
@@ -2,13 +2,23 @@
 Написать функцию преобразования каждого элемента коллекции пользователей в String в соответствии с заданным лямбда-выражением
 fun convertToStrings(users: List<User>, conversion: (User) -> String): List<String>
 
-Дата класс Userуже описан в отдельном файле в этом разделе и доступен здесь.
+Дата класс User уже описан в отдельном файле в этом разделе и доступен здесь.
 
 Проверить работу написанной функции на примере каких-нибудь входных данных.
 
 Для этой задачи есть видео с разбором.
 */
 
-fun main() {
+import User
 
+fun main() {
+    
+    val user1 = User("Lee", 32)
+    val user2 = User("Den", 48)
+    val user3 = User("Joe", 33)
+    val user4 = User("Sue", 45)
+    val user5 = User("JoeS", 33)
+    val user6 = User("SueS", 45)
+    val user7 = User("LeeL", 32)
+    
 }
