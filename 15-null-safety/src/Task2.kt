8dbab1ab950ae2d2e9791709d56eb2ea7e154fd6
KMV <9117479@gmail.com>
2022-03-29 17:22:24 +0300

    15 Ts2

--- 15-null-safety/src/Task2.kt
+++ 15-null-safety/src/Task2.kt
@@ -13,7 +13,25 @@ class Employee(val name: String, val company: Company?)
 */
 
 fun main() {
+    val addres1 = Address("1 Street", "NY")
+    val addres2 = Address("2 Street", "Boston")
+    val addres3 = Address("5 Avenu", "Atlanta")
 
+    val company1 = Company("IBM", addres1)
+    val company2 = Company("Apple", addres2)
+    val company3 = Company("Apple", addres3)
+
+    val user1 = Employee("Lee", company1)
+    val user2 = Employee("Den", company1)
+    val user3 = Employee("Joe", company2)
+    val user4 = Employee("Sue", null)
+
+    println(employeeCity(user4))
+
+}
+fun employeeCity(employee: Employee): String{
+    val result = employee.company?.address?.city ?: "Unknown"
+    return result
 }
 
 class Address(val address: String, val city: String)
